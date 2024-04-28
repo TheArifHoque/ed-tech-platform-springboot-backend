266b6c7ff8aae9d351c3bda1c8a1f1b8fc73c561
TheArifHoque <97823073+TheArifHoque@users.noreply.github.com>
2024-04-28 23:18:00 +0600

    CommonConstant updated

--- common-module/src/main/java/com/arifhoque/commonmodule/constant/CommonConstant.java
+++ common-module/src/main/java/com/arifhoque/commonmodule/constant/CommonConstant.java
@@ -1,10 +1,9 @@
 package com.arifhoque.commonmodule.constant;
 
 public class CommonConstant {
-    private static final String REQUEST_ID = "Request-Id";
-    private static final String CONTENT_TYPE_HEADER_KEY = "Content-Type";
-    private static final String AUTHORIZATION_HEADER = "Authorization";
-    private static final String BEARER_PREFIX = "Bearer ";
+    public static final String REQUEST_ID = "Request-Id";
+    public static final String CONTENT_TYPE_HEADER_KEY = "Content-Type";
 
-    private CommonConstant() {}
+    private CommonConstant() {
+    }
 }
