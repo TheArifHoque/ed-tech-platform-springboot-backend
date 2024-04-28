d51a993f3db0137ed953de64636877e124a75df0
TheArifHoque <97823073+TheArifHoque@users.noreply.github.com>
2024-04-28 12:23:41 +0600

    added common constant file

---
+++ common-module/src/main/java/com/arifhoque/commonmodule/constant/CommonConstant.java
@@ -0,0 +1,11 @@
+package com.arifhoque.commonmodule.constant;
+
+public class CommonConstant {
+    private static final String REQUEST_ID = "Request-Id";
+    private static final String CONTENT_TYPE_HEADER_KEY = "Content-Type";
+    private static final String ACCEPT_HEADER_KEY = "Accept";
+    private static final String AUTHORIZATION_HEADER = "Authorization";
+    private static final String BEARER_PREFIX = "Bearer ";
+
+    private CommonConstant() {}
+}
