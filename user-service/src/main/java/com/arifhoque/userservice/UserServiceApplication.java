7a7cd1bd2b8ab42a51a49e89382cdd96588fea99
TheArifHoque <97823073+TheArifHoque@users.noreply.github.com>
2024-05-06 01:01:51 +0600

    enable discover client

--- user-service/src/main/java/com/arifhoque/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/arifhoque/userservice/UserServiceApplication.java
@@ -2,8 +2,10 @@ package com.arifhoque.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication
+@EnableDiscoveryClient
+@SpringBootApplication(scanBasePackages = "com.arifhoque")
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
