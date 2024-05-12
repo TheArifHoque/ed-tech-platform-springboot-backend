f096dda586f3b09b5cd1fabd708fe30ac6600f6d
TheArifHoque <97823073+TheArifHoque@users.noreply.github.com>
2024-05-12 14:24:29 +0600

    enabled discovery client

--- course-management-service/src/main/java/com/arifhoque/coursemanagementservice/CourseManagementServiceApplication.java
+++ course-management-service/src/main/java/com/arifhoque/coursemanagementservice/CourseManagementServiceApplication.java
@@ -2,8 +2,10 @@ package com.arifhoque.coursemanagementservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication
+@EnableDiscoveryClient
+@SpringBootApplication(scanBasePackages = "com.arifhoque")
 public class CourseManagementServiceApplication {
 
     public static void main(String[] args) {
