86cad7759ea1a238fd3015ad0ff7b07854cd77a3
TheArifHoque <97823073+TheArifHoque@users.noreply.github.com>
2024-05-12 15:54:23 +0600

    file renamed

--- course-management-service/src/main/java/com/arifhoque/coursemanagementservice/repository/CourseEnrollmentRepository.java
+++ course-management-service/src/main/java/com/arifhoque/coursemanagementservice/repository/CourseEnrollmentRepository.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.UUID;
 
 @Repository
-public interface CourseEnrollementRepository extends JpaRepository<CourseEnrollmentInfo, UUID> {
+public interface CourseEnrollmentRepository extends JpaRepository<CourseEnrollmentInfo, UUID> {
 
     List<CourseEnrollmentInfo> findByCourseId(UUID courseId);
 
