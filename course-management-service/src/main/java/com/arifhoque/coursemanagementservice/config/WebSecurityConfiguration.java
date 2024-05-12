68d9023d64c7c99caa08a0a546b5f1f88ad04fd8
TheArifHoque <97823073+TheArifHoque@users.noreply.github.com>
2024-05-12 14:43:45 +0600

    added java doc

--- course-management-service/src/main/java/com/arifhoque/coursemanagementservice/config/WebSecurityConfiguration.java
+++ course-management-service/src/main/java/com/arifhoque/coursemanagementservice/config/WebSecurityConfiguration.java
@@ -10,16 +10,29 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
+/**
+ * Configuration class for Spring Security.
+ *
+ * @author Ariful Hoque
+ */
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
 public class WebSecurityConfiguration {
 
+    /**
+     * Defines a custom security filter chain configuration for Spring Security.
+     * This configuration specifies how to handle security aspects of the application.
+     *
+     * @param httpSecurity the HttpSecurity object to configure the security filters.
+     * @return instance of SecurityFilterChain that defines the order and behavior of security filters.
+     * @throws Exception if an error occurs during configuration.
+     */
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
                 .csrf(AbstractHttpConfigurer::disable)
-                .headers( headersConfigurer -> headersConfigurer
+                .headers(headersConfigurer -> headersConfigurer
                         .frameOptions(HeadersConfigurer.FrameOptionsConfig::disable)
                 )
                 .authorizeHttpRequests(requestMatcherRegistry -> requestMatcherRegistry
