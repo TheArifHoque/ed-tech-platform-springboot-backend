3220274c15a188a8f6a60f809b854cfe11ef731d
TheArifHoque <97823073+TheArifHoque@users.noreply.github.com>
2024-05-12 14:06:49 +0600

    dependency for course-management-service added

---
+++ course-management-service/src/test/java/com/arifhoque/coursemanagementservice/CourseManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arifhoque.coursemanagementservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseManagementServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
