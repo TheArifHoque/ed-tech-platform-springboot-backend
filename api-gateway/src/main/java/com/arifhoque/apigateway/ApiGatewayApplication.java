b8fcee8ad18e30d950eef8466d09e14805d944bd
TheArifHoque <97823073+TheArifHoque@users.noreply.github.com>
2024-05-19 14:12:12 +0600

    enabled discovery client

--- api-gateway/src/main/java/com/arifhoque/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/arifhoque/apigateway/ApiGatewayApplication.java
@@ -2,8 +2,10 @@ package com.arifhoque.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication
+@EnableDiscoveryClient
+@SpringBootApplication(scanBasePackages = "com.arifhoque")
 public class ApiGatewayApplication {
 
     public static void main(String[] args) {
