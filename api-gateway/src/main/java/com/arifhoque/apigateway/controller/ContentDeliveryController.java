d663b2e95e5259bdcdf60afca0867942cef95bc5
TheArifHoque <97823073+TheArifHoque@users.noreply.github.com>
2024-06-27 00:12:47 +0600

    content api and delivery service method added to save content

--- api-gateway/src/main/java/com/arifhoque/apigateway/controller/ContentDeliveryController.java
+++ api-gateway/src/main/java/com/arifhoque/apigateway/controller/ContentDeliveryController.java
@@ -1,13 +1,23 @@
 package com.arifhoque.apigateway.controller;
 
 import com.arifhoque.apigateway.service.ContentAPIService;
+import com.arifhoque.commonmodule.model.CustomHttpResponse;
 import com.arifhoque.commonmodule.util.ResponseBuilder;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+import java.util.Map;
+
+import static com.arifhoque.commonmodule.constant.CommonConstant.AUTHORIZATION_HEADER;
 
 @RestController
 @RequestMapping("/content-api")
@@ -23,9 +33,22 @@ public class ContentDeliveryController {
     public ResponseEntity<?> getContent(@PathVariable String contentUrl) {
         try {
             return contentAPIService.serveContent(contentUrl);
-        } catch (Exception e) {
+        } catch (Exception ex) {
             return ResponseBuilder.buildFailureResponse(HttpStatus.NOT_FOUND, "404",
-                    "Failed to fetch the content! Reason: " + e.getMessage());
+                    "Failed to fetch the content! Reason: " + ex.getMessage());
         }
+    }
+
+    @PostMapping
+    public ResponseEntity<CustomHttpResponse> saveContents(@RequestHeader(AUTHORIZATION_HEADER) String accessToken,
+                                                           @RequestParam MultipartFile[] contents) {
+        List<String> urlList;
+        try {
+            urlList = contentAPIService.saveContents(contents, accessToken);
+        } catch (Exception ex) {
+            return ResponseBuilder.buildFailureResponse(HttpStatus.BAD_REQUEST, "400",
+                    "Failed to save contents in the file system! Reason: " + ex.getMessage());
+        }
+        return ResponseBuilder.buildSuccessResponse(HttpStatus.OK, Map.of("urlList", urlList));
     }
 }
