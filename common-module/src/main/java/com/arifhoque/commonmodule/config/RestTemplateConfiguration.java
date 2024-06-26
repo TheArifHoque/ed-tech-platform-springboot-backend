90f3e314174daad4e601734efc6125a26a112944
TheArifHoque <97823073+TheArifHoque@users.noreply.github.com>
2024-06-27 00:20:22 +0600

    load balancer added

--- common-module/src/main/java/com/arifhoque/commonmodule/config/RestTemplateConfiguration.java
+++ common-module/src/main/java/com/arifhoque/commonmodule/config/RestTemplateConfiguration.java
@@ -2,6 +2,7 @@ package com.arifhoque.commonmodule.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -15,8 +16,9 @@ public class RestTemplateConfiguration {
     public Integer timeout;
 
     @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplateBuilder()
+    @LoadBalanced
+    public RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder) {
+        return restTemplateBuilder
                 .setConnectTimeout(Duration.ofMillis(timeout))
                 .build();
     }
