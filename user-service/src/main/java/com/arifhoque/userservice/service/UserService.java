aabf4926472dbc20cdf9386f3c0b865eede14a35
TheArifHoque <97823073+TheArifHoque@users.noreply.github.com>
2024-06-27 01:31:47 +0600

    added new UserService methods

--- user-service/src/main/java/com/arifhoque/userservice/service/UserService.java
+++ user-service/src/main/java/com/arifhoque/userservice/service/UserService.java
@@ -14,49 +14,92 @@ import static com.arifhoque.userservice.constant.UserServiceConstant.VALID_PASSW
 
 @Service
 public class UserService {
-    private final UserRepo userRepo;
 
-    public UserService(UserRepo userRepo) {
-        this.userRepo = userRepo;
+    private final UserRepo userRepository;
+
+    /**
+     * Constructor for UserService class.
+     *
+     * @param userRepository instance of UserRepository interface.
+     */
+    public UserService(UserRepo userRepository) {
+        this.userRepository = userRepository;
     }
 
+    /**
+     * Retrieves a user by their UUID.
+     *
+     * @param userId the UUID of the user to retrieve.
+     * @return user information associated with the UUID.
+     */
     public User getUser(UUID userId) {
-        return userRepo.findById(userId).orElse(null);
+        return userRepository.findByUserId(userId);
     }
 
+    /**
+     * Retrieves a list of users based on their UUIDs.
+     *
+     * @param userIds list of UUIDs for users to retrieve.
+     * @return list of user information associated with the UUIDs.
+     */
     public List<User> getListOfUser(List<UUID> userIds) {
-        return userRepo.findByUserIdIn(userIds);
+        return userRepository.findByUserIdIn(userIds);
     }
 
+    /**
+     * Adds a new user to the 'user-service-db'.
+     *
+     * @param user the user object representing the new user to add.
+     * @throws Exception if a user with the same email already exists.
+     */
     public void addUser(User user) throws Exception {
-        User existingUser = userRepo.findByEmail(user.getEmail());
+        User existingUser = userRepository.findByEmail(user.getEmail());
         if (existingUser != null) {
             throw new Exception("User with email id - " + user.getEmail() + " already exists!");
         }
-        userRepo.save(user);
+        userRepository.save(user);
     }
 
+    /**
+     * Updates the user's information.
+     *
+     * @param user user data with updated information.
+     * @throws Exception if the user is not found.
+     */
     public void updateUser(User user) throws Exception {
-        User existingUser = userRepo.findByEmail(user.getEmail());
+        User existingUser = userRepository.findByUserId(user.getUserId());
         if (existingUser == null) {
-            throw new Exception("User with email id - " + user.getEmail() + " does not exists!");
+            throw new Exception("User with id - " + user.getUserId() + " not found!");
         }
         existingUser.setFirstName(user.getFirstName());
         existingUser.setLastName(user.getLastName());
         existingUser.setGender(user.getGender());
         existingUser.setDateOfBirth(user.getDateOfBirth());
-        userRepo.save(existingUser);
+        userRepository.save(existingUser);
     }
 
+    /**
+     * Updates the user's profile image.
+     *
+     * @param userId   UUID of the user for whom to update the profile image.
+     * @param imageUrl new image location url.
+     * @throws Exception if the user is not found or an error occurs during the image update.
+     */
     public void updateProfileImage(UUID userId, String imageUrl) throws Exception {
-        User existingUser = userRepo.findById(userId).orElse(null);
+        User existingUser = userRepository.findByUserId(userId);
         if (existingUser == null) {
-            throw new Exception("User with id - " + userId + " does not exists!");
+            throw new Exception("User with id - " + userId + " not found!");
         }
         existingUser.setImageUrl(imageUrl);
-        userRepo.save(existingUser);
+        userRepository.save(existingUser);
     }
 
+    /**
+     * Validates the format of an email address.
+     *
+     * @param email the email address that needs to be validated.
+     * @throws Exception if the email is not in a valid format.
+     */
     public void validateEmail(String email) throws Exception {
         boolean isEmailValid = isEmailValid(email);
         if (!isEmailValid) {
@@ -64,23 +107,43 @@ public class UserService {
         }
     }
 
-    private boolean isEmailValid(String email) {
-        if (!StringUtils.hasLength(email))
-            return false;
-        return Pattern.compile(VALID_EMAIL_REGEX).matcher(email).matches();
-    }
-
+    /**
+     * Validates the format of a password.
+     *
+     * @param password the password that needs to be validated.
+     * @throws Exception if the password is not in a valid format.
+     */
     public void validatePassword(String password) throws Exception {
         boolean isPasswordValid = isPasswordValid(password);
         if (!isPasswordValid) {
-            throw new Exception("Password should be minimum eight characters. and contain at least one uppercase, " +
-                    "one lowercase, one digit, one special character.");
+            throw new Exception("Password should be minimum eight characters. And contain at least one uppercase " +
+                    "letter, one lowercase letter, one number and one special character!");
         }
     }
 
-    private boolean isPasswordValid(String password) {
-        if (!StringUtils.hasLength(password))
+    /**
+     * Checks if the provided email address matches to the defined regex format.
+     *
+     * @param email the email address that needs to be validated.
+     * @return true if it matches to the regex format. Else returns false.
+     */
+    private boolean isEmailValid(String email) {
+        if (!StringUtils.hasLength(email)) {
             return false;
+        }
+        return Pattern.compile(VALID_EMAIL_REGEX).matcher(email).matches();
+    }
+
+    /**
+     * Checks if the provided password matches to the defined regex format.
+     *
+     * @param password the password that needs to be validated.
+     * @return true if it matches to the regex format. Else returns false.
+     */
+    private boolean isPasswordValid(String password) {
+        if (!StringUtils.hasLength(password)) {
+            return false;
+        }
         return Pattern.compile(VALID_PASSWORD_REGEX).matcher(password).matches();
     }
 }
