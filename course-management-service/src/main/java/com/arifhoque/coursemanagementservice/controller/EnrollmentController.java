a6e16819d99b054662583d154b09c933f29da14e
Ariful Hoque <ariful@brainstation-23.com>
2024-12-17 01:11:22 +0600

    renamed CourseEnrollmentController class and refactored

--- course-management-service/src/main/java/com/arifhoque/coursemanagementservice/controller/EnrollmentController.java
+++ course-management-service/src/main/java/com/arifhoque/coursemanagementservice/controller/EnrollmentController.java
@@ -2,8 +2,8 @@ package com.arifhoque.coursemanagementservice.controller;
 
 import com.arifhoque.commonmodule.model.CustomHttpResponse;
 import com.arifhoque.commonmodule.util.ResponseBuilder;
-import com.arifhoque.coursemanagementservice.model.CourseEnrollmentInfo;
-import com.arifhoque.coursemanagementservice.service.CourseEnrollmentService;
+import com.arifhoque.coursemanagementservice.model.EnrollmentInfo;
+import com.arifhoque.coursemanagementservice.service.EnrollmentService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -17,40 +17,42 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Map;
 import java.util.UUID;
 
+import static com.arifhoque.commonmodule.constant.CommonConstant.MESSAGE;
+
 @RestController
-@RequestMapping("/course-enrollment")
-public class CourseEnrollmentController {
+@RequestMapping("/enrollment")
+public class EnrollmentController {
 
-    private final CourseEnrollmentService courseEnrollmentService;
+    private final EnrollmentService enrollmentService;
 
-    public CourseEnrollmentController(CourseEnrollmentService courseEnrollmentService) {
-        this.courseEnrollmentService = courseEnrollmentService;
+    public EnrollmentController(EnrollmentService enrollmentService) {
+        this.enrollmentService = enrollmentService;
     }
 
-    @PostMapping("/enroll")
+    @PostMapping
     @PreAuthorize("hasRole('ADMIN')")
-    public ResponseEntity<CustomHttpResponse> enrollToCourse(@RequestBody CourseEnrollmentInfo courseEnrollmentInfo) {
+    public ResponseEntity<CustomHttpResponse> enrollToCourse(@RequestBody EnrollmentInfo enrollmentInfo) {
         try {
-            courseEnrollmentService.enrollToCourse(courseEnrollmentInfo);
+            enrollmentService.enrollToCourse(enrollmentInfo);
         } catch (Exception ex) {
             return ResponseBuilder.buildFailureResponse(HttpStatus.BAD_REQUEST, "400",
                     "Failed to enroll to the course! Reason: " + ex.getMessage());
         }
-        return ResponseBuilder.buildSuccessResponse(HttpStatus.CREATED,
-                Map.of("message", "Successfully enrolled to the course"));
+        return ResponseBuilder.buildSuccessResponse(HttpStatus.OK, Map.of(MESSAGE,
+                "Successfully enrolled to the course"));
     }
 
-    @GetMapping("/enroll/courses")
+    @GetMapping("/courses")
     @PreAuthorize("hasAnyRole('ADMIN','USER')")
     public ResponseEntity<CustomHttpResponse> getAllEnrolledCourseIdsWithStatus(@RequestParam UUID userId) {
         Map<UUID, String> enrolledCourseIdsWithStatusMap;
         try {
-            enrolledCourseIdsWithStatusMap = courseEnrollmentService.getEnrolledCourseIdsWithStatus(userId);
+            enrolledCourseIdsWithStatusMap = enrollmentService.getEnrolledCourseIdsWithStatus(userId);
         } catch (Exception ex) {
             return ResponseBuilder.buildFailureResponse(HttpStatus.BAD_REQUEST, "400",
                     "Failed to fetch enrolled course id list with status! Reason: " + ex.getMessage());
         }
-        return ResponseBuilder.buildSuccessResponse(HttpStatus.OK, Map.of("enrolledCourseIdsWithStatusMap",
+        return ResponseBuilder.buildSuccessResponse(HttpStatus.OK, Map.of("enrolledCourseIdsWithStatus",
                 enrolledCourseIdsWithStatusMap));
     }
 }
