a0cb9779a61f965f99a787e26a0983d0a93cfd2f
Ariful Hoque <ariful@brainstation-23.com>
2024-12-21 22:24:58 +0600

    HttpCallLogic refactored

--- common-module/src/main/java/com/arifhoque/commonmodule/util/HttpCallLogic.java
+++ common-module/src/main/java/com/arifhoque/commonmodule/util/HttpCallLogic.java
@@ -1,103 +1,156 @@
 package com.arifhoque.commonmodule.util;
 
+import com.arifhoque.commonmodule.exception.ApiException;
 import com.arifhoque.commonmodule.model.CustomHttpRequest;
 import com.arifhoque.commonmodule.model.CustomHttpResponse;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.HttpServerErrorException;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.HashMap;
 import java.util.Map;
 
 import static com.arifhoque.commonmodule.constant.CommonConstant.CONTENT_TYPE_HEADER_KEY;
+import static com.arifhoque.commonmodule.constant.CommonConstant.ERROR_CODE;
+import static com.arifhoque.commonmodule.constant.CommonConstant.ERROR_MESSAGE;
 import static com.arifhoque.commonmodule.constant.CommonConstant.REQUEST_ID;
 
 @Component
 public class HttpCallLogic {
 
-    private final RestTemplate restTemplate;
-    private final Logger systemLogger;
+    private static final String STATUS = "status";
+    private static final String ERROR = "error";
+    private static final String PATH = "path";
+    private static final String QUESTION_SYMBOL = "?";
+    private static final String EQUAL_SYMBOL = "=";
+    private static final String AND_SYMBOL = "&";
 
-    public HttpCallLogic(@Qualifier("systemLogger") Logger systemLogger, RestTemplate restTemplate) {
+    private final Logger systemLogger;
+    private final RestTemplate restTemplate;
+    private final ObjectMapper objectMapper;
+
+    public HttpCallLogic(@Qualifier("systemLogger") Logger systemLogger, RestTemplate restTemplate,
+                         ObjectMapper objectMapper) {
         this.systemLogger = systemLogger;
         this.restTemplate = restTemplate;
+        this.objectMapper = objectMapper;
+    }
+
+    public Map<String, Object> getHttpResponse(CustomHttpRequest customHttpRequest) {
+        CustomHttpResponse customHttpResponse = executeRequest(customHttpRequest).getBody();
+        if (customHttpResponse != null && customHttpResponse.getResponseBody() != null) {
+            return customHttpResponse.getResponseBody();
+        }
+        return new HashMap<>();
+    }
+
+    public Map<String, Object> getHttpResponseWithException(CustomHttpRequest customHttpRequest) {
+        CustomHttpResponse customHttpResponse = executeRequest(customHttpRequest).getBody();
+        if (customHttpResponse != null && customHttpResponse.getResponseBody() != null) {
+            return customHttpResponse.getResponseBody();
+        } else if (customHttpResponse != null && customHttpResponse.getErrorBody() != null) {
+            HttpStatus httpStatus = customHttpResponse.getHttpStatus();
+            String errorCode = customHttpResponse.getErrorBody().get(ERROR_CODE).toString();
+            String errorMessage = customHttpResponse.getErrorBody().get(ERROR_MESSAGE).toString();
+            throw new ApiException(httpStatus, errorCode, errorMessage);
+        }
+        return new HashMap<>();
     }
 
     public ResponseEntity<CustomHttpResponse> executeRequest(CustomHttpRequest customHttpRequest) {
-        URI url = prepareRequestUri(customHttpRequest);
-        HttpMethod methodType = customHttpRequest.getMethodType();
-        HttpHeaders requestHeaders = prepareRequestHeaders(customHttpRequest);
-        Map<String, ?> requestBody = prepareRequestBody(customHttpRequest);
-        HttpEntity<Map<String, ?>> requestEntity = new HttpEntity<>(requestBody, requestHeaders);
-        return restTemplate.exchange(url, methodType, requestEntity, CustomHttpResponse.class);
+        try {
+            HttpMethod methodType = customHttpRequest.getMethodType();
+            URI url = prepareRequestUri(customHttpRequest);
+            HttpHeaders requestHeaders = prepareRequestHeaders(customHttpRequest);
+            Map<String, ?> requestBody = prepareRequestBody(customHttpRequest);
+            HttpEntity<Map<String, ?>> requestEntity = new HttpEntity<>(requestBody, requestHeaders);
+            return restTemplate.exchange(url, methodType, requestEntity, CustomHttpResponse.class);
+        } catch (HttpClientErrorException | HttpServerErrorException ex) {
+            return handle4xx5xxErrorResponse(ex.getStatusCode(), ex.getResponseBodyAsString());
+        } catch (Exception ex) {
+            String errorMessage = "Error occurred while executing HTTP request! Reason: " + ex.getMessage();
+            systemLogger.error(errorMessage);
+            throw new ApiException(HttpStatus.INTERNAL_SERVER_ERROR, "500", errorMessage);
+        }
     }
 
+    private ResponseEntity<CustomHttpResponse> handle4xx5xxErrorResponse(HttpStatusCode httpStatusCode,
+                                                                         String errorResponseBody) {
+        try {
+            CustomHttpResponse customHttpResponse = objectMapper.readValue(errorResponseBody, CustomHttpResponse.class);
+            if (customHttpResponse.getHttpStatus() == null || customHttpResponse.getErrorBody() == null) {
+                Map errorBody = objectMapper.readValue(errorResponseBody, Map.class);
+                Integer status = (Integer) errorBody.get(STATUS);
+                String error = (String) errorBody.get(ERROR);
+                String path = (String) errorBody.get(PATH);
+                customHttpResponse.setHttpStatus(HttpStatus.valueOf(status));
+                customHttpResponse.setErrorBody(Map.of(ERROR_CODE, status, ERROR_MESSAGE, error + " - " + path));
+            }
+            return new ResponseEntity<>(customHttpResponse, httpStatusCode);
+        } catch (JsonProcessingException ex) {
+            String errorMessage = "Error occurred while handling 4xx/5xx response! Reason: " + ex.getMessage();
+            systemLogger.error(errorMessage);
+            throw new ApiException(HttpStatus.INTERNAL_SERVER_ERROR, "500", errorMessage);
+        }
+    }
+
+    // TODO: Need to refactor this method later
     public ResponseEntity<byte[]> fetchMediaContent(CustomHttpRequest customHttpRequest) {
-        URI url = prepareRequestUri(customHttpRequest);
+        URI url;
+        try {
+            url = prepareRequestUri(customHttpRequest);
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
         HttpMethod methodType = customHttpRequest.getMethodType();
         HttpHeaders requestHeaders = prepareRequestHeaders(customHttpRequest);
         HttpEntity<Map<String, ?>> requestEntity = new HttpEntity<>(requestHeaders);
         return restTemplate.exchange(url, methodType, requestEntity, byte[].class);
     }
 
-    private URI prepareRequestUri(CustomHttpRequest customHttpRequest) {
+    private URI prepareRequestUri(CustomHttpRequest customHttpRequest) throws URISyntaxException {
         String url = customHttpRequest.getUrl();
-        Map<String, String> urlParameterMap = customHttpRequest.getUrlParameterMap();
-        String queryParameters = "";
-        if (urlParameterMap != null && !urlParameterMap.isEmpty()) {
-            url = insertPathVariablesInUrl(url, urlParameterMap);
-            insertPathVariablesInUrl(url, urlParameterMap);
-            queryParameters = buildQueryParameters(url, urlParameterMap);
+        Map<String, String> queryParameterMap = customHttpRequest.getQueryParameterMap();
+        if (queryParameterMap != null && !queryParameterMap.isEmpty()) {
+            String queryParameters = buildQueryParameters(queryParameterMap);
+            return new URI(url + QUESTION_SYMBOL + queryParameters);
         }
-        try {
-            return new URI(url + queryParameters);
-        } catch (URISyntaxException ex) {
-            systemLogger.error("HttpCallLogic: Exception occurred while building URI!");
-        }
-        return null;
+        return new URI(url);
     }
 
-    private String insertPathVariablesInUrl(String url, Map<String, String> urlParameterMap) {
-        for (Map.Entry<String, String> element : urlParameterMap.entrySet()) {
-            String key = element.getKey();
-            String value = element.getValue();
-            String pathVariableKey = String.format("{%s}", key);
-            if (url.contains(pathVariableKey)) {
-                url = url.replace(pathVariableKey, value);
-                urlParameterMap.remove(key);
-            }
-        }
-        return url;
-    }
-
-    private String buildQueryParameters(String url, Map<String, String> urlParameterMap) {
+    private String buildQueryParameters(Map<String, String> queryParameterMap) {
         StringBuilder queryParameters = new StringBuilder();
-        queryParameters.append(url.contains("?") ? "&" : "?");
-        for (Map.Entry<String, String> element : urlParameterMap.entrySet()) {
-            String key = element.getKey();
-            String value = element.getValue();
-            queryParameters.append(key);
-            queryParameters.append("=");
-            queryParameters.append(value);
-            queryParameters.append("&");
+        for (Map.Entry<String, String> queryParameter : queryParameterMap.entrySet()) {
+            queryParameters
+                    .append(queryParameter.getKey())
+                    .append(EQUAL_SYMBOL)
+                    .append(queryParameter.getValue())
+                    .append(AND_SYMBOL);
         }
         return queryParameters.substring(0, queryParameters.length() - 1);
     }
 
     private HttpHeaders prepareRequestHeaders(CustomHttpRequest customHttpRequest) {
+        Map<String, String> headerParameterMap = customHttpRequest.getHeaderParameterMap();
         HttpHeaders httpHeaders = new HttpHeaders();
         httpHeaders.add(REQUEST_ID, customHttpRequest.getRequestId());
-        Map<String, String> headerParameterMap = customHttpRequest.getHeaderParameterMap();
         if (headerParameterMap != null && !headerParameterMap.isEmpty()) {
             for (Map.Entry<String, String> header : headerParameterMap.entrySet()) {
                 httpHeaders.add(header.getKey(), header.getValue());
@@ -107,12 +160,14 @@ public class HttpCallLogic {
     }
 
     private Map<String, ?> prepareRequestBody(CustomHttpRequest customHttpRequest) {
-        Map<String, Object> bodyMap = customHttpRequest.getBodyMap();
-        boolean isMultipartFormDataHeaderPresent = isMultipartFormDataHeaderPresent(customHttpRequest);
-        if (isMultipartFormDataHeaderPresent && bodyMap != null && !bodyMap.isEmpty()) {
-            return prepareMultiValueRequestBody(bodyMap);
+        Map<String, Object> bodyParameterMap = customHttpRequest.getBodyParameterMap();
+        if (bodyParameterMap != null && !bodyParameterMap.isEmpty()) {
+            if (isMultipartFormDataHeaderPresent(customHttpRequest)) {
+                return prepareMultiValueRequestBody(bodyParameterMap);
+            }
+            return bodyParameterMap;
         }
-        return bodyMap;
+        return new HashMap<>();
     }
 
     private boolean isMultipartFormDataHeaderPresent(CustomHttpRequest customHttpRequest) {
@@ -123,15 +178,15 @@ public class HttpCallLogic {
         return false;
     }
 
-    private MultiValueMap<String, Object> prepareMultiValueRequestBody(Map<String, Object> bodyMap) {
+    private MultiValueMap<String, Object> prepareMultiValueRequestBody(Map<String, Object> bodyParameterMap) {
         MultiValueMap<String, Object> requestBody = new LinkedMultiValueMap<>();
-        for (Map.Entry<String, Object> element : bodyMap.entrySet()) {
-            if (element.getValue() instanceof MultipartFile[] multipartFiles) {
+        for (Map.Entry<String, Object> bodyParameter : bodyParameterMap.entrySet()) {
+            if (bodyParameter.getValue() instanceof MultipartFile[] multipartFiles) {
                 for (MultipartFile multipartFile : multipartFiles) {
-                    requestBody.add(element.getKey(), multipartFile.getResource());
+                    requestBody.add(bodyParameter.getKey(), multipartFile.getResource());
                 }
             } else {
-                requestBody.add(element.getKey(), element.getValue());
+                requestBody.add(bodyParameter.getKey(), bodyParameter.getValue());
             }
         }
         return requestBody;
