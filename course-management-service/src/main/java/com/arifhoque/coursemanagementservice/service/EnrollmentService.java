c2883d630cd21e2547d25f301d813e26beb53399
Ariful Hoque <ariful@brainstation-23.com>
2025-03-08 11:32:20 +0600

    updated getEnrollmentStatus

--- course-management-service/src/main/java/com/arifhoque/coursemanagementservice/service/EnrollmentService.java
+++ course-management-service/src/main/java/com/arifhoque/coursemanagementservice/service/EnrollmentService.java
@@ -1,9 +1,11 @@
 package com.arifhoque.coursemanagementservice.service;
 
+import com.arifhoque.coursemanagementservice.model.Course;
 import com.arifhoque.coursemanagementservice.model.EnrollmentInfo;
 import com.arifhoque.coursemanagementservice.repository.EnrollmentInfoRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -13,27 +15,44 @@ import java.util.UUID;
 public class EnrollmentService {
 
     private final EnrollmentInfoRepository enrollmentRepository;
+    private final CourseService courseService;
 
-    public EnrollmentService(EnrollmentInfoRepository enrollmentRepository) {
+    public EnrollmentService(EnrollmentInfoRepository enrollmentRepository, CourseService courseService) {
         this.enrollmentRepository = enrollmentRepository;
+        this.courseService = courseService;
     }
 
-    public void enrollToCourse(EnrollmentInfo enrollmentInfo) {
-        EnrollmentInfo existingEnrollmentInfo = enrollmentRepository.findByCourseIdAndUserId(
-                enrollmentInfo.getCourseId(), enrollmentInfo.getUserId());
-        if (existingEnrollmentInfo != null) {
-            existingEnrollmentInfo.setStatus(enrollmentInfo.getStatus());
+    public void enrollToCourse(UUID courseId, UUID userId, String status) {
+        EnrollmentInfo existingEnrollmentInfo = enrollmentRepository.findByCourseIdAndUserId(courseId, userId);
+        if (existingEnrollmentInfo == null) {
+            existingEnrollmentInfo = new EnrollmentInfo();
+            existingEnrollmentInfo.setCourseId(courseId);
+            existingEnrollmentInfo.setUserId(userId);
         }
-        enrollmentRepository.save(enrollmentInfo);
+        existingEnrollmentInfo.setStatus(status);
+        enrollmentRepository.save(existingEnrollmentInfo);
     }
 
-    public Map<UUID, String> getEnrolledCourseIdsWithStatus(UUID userId) {
+    public List<Course> getEnrolledCourses(UUID userId) {
         List<EnrollmentInfo> enrollmentInfoList = enrollmentRepository.findByUserId(userId);
         Map<UUID, String> enrolledCourseIdsWithStatusMap = new HashMap<>();
         for (EnrollmentInfo enrollmentInfo : enrollmentInfoList) {
             enrolledCourseIdsWithStatusMap.put(enrollmentInfo.getCourseId(), enrollmentInfo.getStatus());
         }
-        return enrolledCourseIdsWithStatusMap;
+        List<UUID> enrolledCourseIds = new ArrayList<>(enrolledCourseIdsWithStatusMap.keySet());
+        List<Course> enrolledCourses = courseService.getListOfCourse(enrolledCourseIds);
+        for (Course enrolledCourse : enrolledCourses) {
+            enrolledCourse.setEnrollmentStatus(enrolledCourseIdsWithStatusMap.get(enrolledCourse.getCourseId()));
+        }
+        return enrolledCourses;
+    }
+
+    public String getEnrollmentStatus(UUID courseId, UUID userId) throws Exception {
+        EnrollmentInfo existingEnrollmentInfo = enrollmentRepository.findByCourseIdAndUserId(courseId, userId);
+        if (existingEnrollmentInfo == null) {
+            throw new Exception("No enrollment info found for user id - " + userId + " and course id - " + courseId);
+        }
+        return existingEnrollmentInfo.getStatus();
     }
 
     public List<UUID> getEnrolledUserIds(UUID courseId) {
